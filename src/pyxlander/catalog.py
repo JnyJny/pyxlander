4e2d8eac717100ffa30b8a90dd3e714bc2a09f6a
JnyJny <erik.oshaughnessy@gmail.com>
2018-11-11 21:01:37 -0600

    extracted sprite class into own file

--- src/pyxlander/catalog.py
+++ src/pyxlander/catalog.py
@@ -2,55 +2,13 @@
 '''
 
 from dataclasses import dataclass
-from copy import copy
+from .bitmap import Bitmap
 
+LanderHull = Bitmap(16, 0, 16, 16)
+Exhaust_D = Bitmap(40, 0, 8, 16)
+Exhaust_R = Bitmap(32, 0, 8, 8)
+Exhaust_L = Bitmap(32, 0, -8, 8)
+Twinkle0 = Bitmap(48, 0, 3, 3)
+Twinkle1 = Bitmap(51, 0, 3, 3)
 
-@dataclass
-class Sprite:
-    x: int = 0
-    y: int = 0
-    sx: int = 0
-    sy: int = 0
-    w: int = 16
-    h: int = 16
-    bank: int = 0    
-    key: int = 0
-
-    def __iter__(self):
-        return iter((self.x, self.y, self.bank,
-                     self.sx, self.sy, self.w, self.h,
-                     self.key))
-
-    def copy(self, x, y):
-        c = copy(self)
-        c.x, c.y = x, y
-        return c
-
-    def draw(self):
-        pyxel.blt(*self)
-
-
-class LanderHull(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 16, 0, 16, 16)
-
-class Exhaust_D(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 40, 0, 8, 16)
-
-class Exhaust_R(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 32, 0, 8, 8)
-
-class Exhaust_L(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 32, 0, -8, 8)
-
-class Twinkle0(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 48, 0, 3, 3)
-
-class Twinkle1(Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y, 51, 0, 3, 3)        
                         
--- src/pyxlander/sprite.py
+++ src/pyxlander/sprite.py
@@ -1,7 +1,15 @@
 '''
 '''
 
+from twod import Point, Rect
 
-class Sprite:
-    def __init__(self, x, y, w, h, image_bank=0):
-        pass
+class Sprite(Point):
+    def __init__(self, x, y, bitmap):
+        super().__init__(x, y)
+        self.bitmap = bitmap
+
+    def __iter__(self):
+        return iter((self.x, self.y, *self.bitmap))
+
+    def draw(self):
+        self.bitmap.draw(self.x, self.y)
